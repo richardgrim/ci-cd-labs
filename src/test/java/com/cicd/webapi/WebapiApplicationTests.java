6a69487ce1e9d924d208e1ce53dabbdca09536a8
Richard <grichukinrichmendez.94@gmail.com>
2026-08-18 22:45:15 -0400

    test: simulate failing health endpoint test

--- src/test/java/com/cicd/webapi/WebapiApplicationTests.java
+++ src/test/java/com/cicd/webapi/WebapiApplicationTests.java
@@ -30,7 +30,7 @@ class WebapiApplicationTests {
 		MockMvc.perform(get("/health")
 				.accept(MediaType.TEXT_PLAIN))
 			.andExpect(status().isOk())
-			.andExpect(content().string("Server Healthy!"));
+			.andExpect(content().string("Server NOT Healthy!"));
 	}
 
 	@Test
